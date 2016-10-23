d78927e4f15c1fec9c7b6fd35700d1b07aec1e14
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2016-10-23 21:14:30 +0000

    Adding static pages layouts, controllers and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-  get 'home/index'
-
-  root :to => "home#index"
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  root :to => "static_pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
