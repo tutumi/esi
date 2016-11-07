ee7ab893f20f38b63f47e23a7faf667506f03a66
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2016-11-07 22:36:23 +0000

    Static page test

---
+++ spec/views/static_pages/index.html.erb_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "static_pages/index.html.erb", type: :view do
+   it "renders links" do
+    render
+    assert_select "a", :href => root_path
+    assert_select "a", :href => about_path
+    assert_select "a", :href => contact_path
+    assert_select "a", :href => login_path
+  end
+end
