8d03d2c1faef0a1987aaa2fc93e39e7292cde6b0
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2016-11-14 23:12:35 +0000

    updates static pages view test

--- spec/views/static_pages/index.html.erb_spec.rb
+++ spec/views/static_pages/index.html.erb_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "static_pages/index.html.erb", type: :view do
     assert_select "a", :href => root_path
     assert_select "a", :href => about_path
     assert_select "a", :href => contact_path
-    assert_select "a", :href => login_path
+    assert_select "a", :href => login_path, :count => 2
+    assert_select "a", :href => signup_path
   end
 end
