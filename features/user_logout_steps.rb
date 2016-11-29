8cdab0b3193acf58d06c0eb7bdcaa4fa23e16c9a
anactc <anactrino@gmail.com>
2016-11-29 17:16:25 +0000

    .

--- features/user_logout_steps.rb
+++ features/user_logout_steps.rb
@@ -1,5 +1,5 @@
-## Given (/^I am logged in$/) do
-##  curso = Course.create({'nome' => 'SI', 'codcg' => 9999, 'codcur' => 999999, 'codhab' => 999, 'duracao_min' => 8, 'duracao_ideal' => 8, 'duracao_max' => 12, 'ch_obrigatoria_aula' => 1830, 'ch_obrigatoria_trab' => 720, 'ch_eletiva_aula' => 240, 'ch_eletiva_trab' => 0, 'ch_livre_aula' => 750, 'ch_livre_trab' => 0, 'ch_estagio' => 480, 'periodo' => 'noturno'})
-##  User.create('name' => 'Sergio Tutumi', 'email' => 'sergio@tutumi.com.br', 'nusp' => '123456', 'password' => 'senha123', 'course' => curso)
-##  User.destroy
-##end
+#Given (/^I am logged in$/) do
+#  curso = Course.create({'nome' => 'SI', 'codcg' => 9999, 'codcur' => 999999, 'codhab' => 999, 'duracao_min' => 8, 'duracao_ideal' => 8, 'duracao_max' => 12, 'ch_obrigatoria_aula' => 1830, 'ch_obrigatoria_trab' => 720, 'ch_eletiva_aula' => 240, 'ch_eletiva_trab' => 0, 'ch_livre_aula' => 750, 'ch_livre_trab' => 0, 'ch_estagio' => 480, 'periodo' => 'noturno'})
+#  User.create('name' => 'Sergio Tutumi', 'email' => 'sergio@tutumi.com.br', 'nusp' => '123456', 'password' => 'senha123', 'course' => curso)
+#  User.destroy
+#end
